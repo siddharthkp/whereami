36cd562c77b1024ca7e256c1d358762daf9964e8
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2014-09-21 10:10:05 +0530

    Update topdishes.js

--- topdishes.js
+++ topdishes.js
@@ -70,7 +70,10 @@ function getMenuImages(place) {
     console.log('fetching menu images');
     var url = 'http://localhost:8081/menuphotos?name=' + encodeURIComponent(place.name) + '&location=' + place.location;
     request(url, function (error, response, data) {
-        menuImage = data.menuImages[0];
+        if (typeof data === 'string') {
+          data = JSON.parse(data);
+        }
+        var menuImage = data.menuImages[0];
         getMeMenu(menuImage, place);
     });
 }

5a094c02aae501df8508c8ce35b5671aa555676e
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2014-09-21 07:21:31 +0530

    Update whereami.js

--- whereami.js
+++ whereami.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app     = express();
+
+var request = require('request');
+
  
 app.get('/whereami', function(req, res){
     var location = req.query.location;
@@ -13,6 +16,7 @@ exports = module.exports = app;
 
 //location = 12.9669362,77.5953564
 var https = require('https');
+var https2 = require('https');
 var key = 'AIzaSyArBmLVB_OqHZAiQo7zoSzbnAiDjkPZ03o';
 var radius = 25;
 var host = 'maps.googleapis.com';
@@ -22,22 +26,16 @@ var path = '/maps/api/place/nearbysearch/json?types=food' + '&key=' + key + '&ra
 var res = null;
 function getPlaces (location, responder) {
   res = responder;
-  path += '&location=' + location;
-    var options = {
-        host: host,
-        path: path,
-    };
-  fetch(options);
+  fetch(location);
 }
 
-function fetch(options) {
-    console.log('getting name');
-    var callback = function(response) {
-        var data = '';
-        response.on('data', function(chunk) {
-            data += chunk;
-        });
-        response.on('end', function() {
+function fetch(location) {
+    var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?types=food'; 
+    url += '&key=' + key;
+    url += '&radius=' + radius;
+    url += '&location=' + location;
+    request(url, function (error, response, data) {
+        if (!error && response.statusCode == 200) {
             data = JSON.parse(data);
             var place;
             if (data.results.length > 1) {
@@ -47,46 +45,11 @@ function fetch(options) {
             }
             if (!place) {
                 res.send(null);
-                return;
             }
             var name = place.name;
-            console.log('got name');
-            respond('name', name);
-            getLocation(place.place_id);
-        });
-    }
-    https.get(options, callback).end();
-}
-
-function getLocation(placeId) {
-    console.log('getting location');
-    var path = '/maps/api/place/details/json?placeid=' + placeId + '&key=' + key;
-    var options = {
-        host: host,
-        path: path
-    };
-    var callback = function(response) {
-        var data = '';
-        response.on('data', function(chunk) {
-            data += chunk;
-        });
-        response.on('end', function() {
-            data = JSON.parse(data);
-            console.log('got location');
-            respond('address', data.result.formatted_address);
-        });
-    }
-    https.get(options, callback).end();
-}
-
-var response = {
-    name: null,
-    address: null
-}
-
-function respond(type, value) {
-    response[type] = value;
-    if (response.name && response.address) {
-      res.send(response);
-    }
+            res.send({
+                'name': name
+            });
+        }
+    });
 }

f66ae8b957014282628d223ca5da9e8e94a81d68
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2014-09-21 10:14:22 +0530

    Update topdishes.js

--- topdishes.js
+++ topdishes.js
@@ -46,11 +46,14 @@ function fetch(location) {
                 name: data.results[0].name,
                 location: data.results[0].vicinity
             }
-            if (place.name === 'K & K' || place.name === 'Hunan') {
-                if (place.name === 'Hunun') {
-                    place.name = 'Moscow Mule';
+            if (places[i].name === 'K & K' || places[i].name === 'Hunan') {
+                if (places[i].name === 'Hunan') {
+                    places[i].name = 'Moscow Mule';
                 }
-            }
+                var temp = places[i];
+                places.splice(i, 1);
+                places.unshift(temp)
+            };
             getReviews(place);
         }
     });
